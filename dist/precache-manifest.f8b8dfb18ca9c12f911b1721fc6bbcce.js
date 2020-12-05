5ec2779bcc045c9370fe9d26bdd5a23612edc2a6
anhhuy1020 <hoquochuy1998@gmail.com>
2020-12-05 15:35:16 +0700

    Initial dist subtree commit

---
+++ dist/precache-manifest.f8b8dfb18ca9c12f911b1721fc6bbcce.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "384cacd8a26caa02755f",
+    "url": "/vehicles-saver-admin-panel/css/app.dd493f5c.css"
+  },
+  {
+    "revision": "384cacd8a26caa02755f",
+    "url": "/vehicles-saver-admin-panel/js/app.8663b32b.js"
+  },
+  {
+    "revision": "e2d928018cb3ba2a805c",
+    "url": "/vehicles-saver-admin-panel/js/chunk-2d219ff9.0aab29fd.js"
+  },
+  {
+    "revision": "1ce948710e51de52ce9d",
+    "url": "/vehicles-saver-admin-panel/css/chunk-vendors.930339d9.css"
+  },
+  {
+    "revision": "1ce948710e51de52ce9d",
+    "url": "/vehicles-saver-admin-panel/js/chunk-vendors.b0fe56fc.js"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/vehicles-saver-admin-panel/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/vehicles-saver-admin-panel/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/vehicles-saver-admin-panel/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/vehicles-saver-admin-panel/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "c49ec9b0109acd8feeda3d19abd82900",
+    "url": "/vehicles-saver-admin-panel/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/vehicles-saver-admin-panel/.gitkeep"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/vehicles-saver-admin-panel/img/default-avatar.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/vehicles-saver-admin-panel/favicon.png"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/vehicles-saver-admin-panel/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/vehicles-saver-admin-panel/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/vehicles-saver-admin-panel/Dashboard.PNG"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/vehicles-saver-admin-panel/img/favicon.png"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/vehicles-saver-admin-panel/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/vehicles-saver-admin-panel/img/mask.png"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/vehicles-saver-admin-panel/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/vehicles-saver-admin-panel/img/new_logo.png"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/vehicles-saver-admin-panel/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/vehicles-saver-admin-panel/img/sidebar-1.jpg"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/vehicles-saver-admin-panel/img/vue-logo.png"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/vehicles-saver-admin-panel/img/tim_80x80.png"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/vehicles-saver-admin-panel/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/vehicles-saver-admin-panel/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vehicles-saver-admin-panel/robots.txt"
+  }
+];
